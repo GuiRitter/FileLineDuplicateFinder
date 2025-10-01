224c37977e14a2adbf596f535cb843ebddafcbe5
Guilherme Alan Ritter <gui.a.ritter@gmail.com>
2025-10-01 12:26:23 -0300

    bootstrapped project

---
+++ src/main/java/io/github/guiritter/file_line_duplicate_finder/FileLineDuplicateFinder.java
@@ -0,0 +1,8 @@
+package io.github.guiritter.file_line_duplicate_finder;
+
+public class FileLineDuplicateFinder {
+
+	public static void main(String args[]) {
+		System.out.println("Hello, World!");
+	}
+}
